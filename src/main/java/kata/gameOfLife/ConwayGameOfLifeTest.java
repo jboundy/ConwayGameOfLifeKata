e0ac81cedd316f6a978cce5874abe1202a5f6b2c
Michael Sanders <mesanders@mail.bradley.edu>
2015-12-03 19:56:11 -0600

    Updated build path and used it to run tests

--- src/main/java/kata/gameOfLife/ConwayGameOfLifeTest.java
+++ src/main/java/kata/gameOfLife/ConwayGameOfLifeTest.java
@@ -2,10 +2,13 @@
 
 public class ConwayGameOfLifeTest {
 	private ConwayGameOfLife cgol = new ConwayGameOfLife();
+	public static final String cut = "ConwayGameOfLife.";
 
-	
+	public static void pmr(String runningtest) {
+		System.out.println("Running " + cut + " " + runningtest);
+	}
 	public static void testGrid() {
-
+		pmr("testGrid()");			
 	}
 
 	public void runTests() {
