46779c1c2ec93fbb9cbce1e8a77aac25897eb1c2
Michael Sanders <mesanders@mail.bradley.edu>
2015-12-03 19:39:50 -0600

    Added build.xml file for ant. Created Directory structure. Successfully built

---
+++ src/main/java/kata/gameOfLife/ConwayGameOfLifeTest.java
@@ -0,0 +1,19 @@
+
+
+public class ConwayGameOfLifeTest {
+	private ConwayGameOfLife cgol = new ConwayGameOfLife();
+
+	
+	public static void testGrid() {
+
+	}
+
+	public void runTests() {
+		testGrid();
+
+	}
+
+	public static void main(String[] args) {
+		new ConwayGameOfLifeTest().runTests();
+	}
+}
---
+++ src/test/java/kata/gameOfLife/ConwayGameOfLifeTest.java
@@ -0,0 +1,8 @@
+import kata.ConwayGameOfLife;
+
+public class ConwayGameOfLifeKata {
+	public static void main() {
+		
+	}
+}
+
