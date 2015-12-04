5bcfbb3d37ac345edad4c52b5ce1cc5408402c0e
Michael Sanders <mesanders@mail.bradley.edu>
2015-12-03 21:31:17 -0600

    Updated the README file. build.xml and the GridTest. Verified that assertions worked. Tested the Grid to make sure that the grid is properly populated from the file. Right now the grid is a set size, but we will change to a dynamic grid later.

--- src/main/java/kata/gameOfLife/GridTest.java
+++ src/main/java/kata/gameOfLife/GridTest.java
@@ -6,12 +6,14 @@ import java.util.List;
 import java.io.IOException;
 
 public class GridTest {
+ private Grid grid = new Grid();
+ private static String TESTFILE = "default_ingest.txt";
 
-  private boolean[][] grid = new boolean[4][8];
-  private boolean IsAlive;
-  private boolean IsDead;
-
-  public boolean ReadGridTest() throws IOException {
-      return false;
+  public void ReadGridTest() throws IOException {
+    	boolean[][] readgrid = grid.ReadGrid(TESTFILE); 
+   	assert(readgrid[1][4] == true);
+	assert(readgrid[1][3] == false);
+	assert(readgrid[1][5] == false);
+	assert(readgrid[2][4] == true);
   }
 }
