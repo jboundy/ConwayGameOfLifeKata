732a2bec7bd151d484e01028ae61d017812199c9
Justin Boundy <jboundy@advancedtech.com>
2015-12-03 20:49:18 -0600

    Setup Grid class

--- src/main/java/kata/gameOfLife/ConwayGameOfLifeTest.java
+++ src/main/java/kata/gameOfLife/ConwayGameOfLifeTest.java
@@ -1,22 +1,29 @@
-
+import java.io.IOException;
 
 public class ConwayGameOfLifeTest {
+	private static final String TESTFILE = "bin/default_ingest.txt";
 	private ConwayGameOfLife cgol = new ConwayGameOfLife();
 	public static final String cut = "ConwayGameOfLife.";
 
 	public static void pmr(String runningtest) {
 		System.out.println("Running " + cut + " " + runningtest);
 	}
-	public static void testGrid() {
-		pmr("testGrid()");			
+
+	public static void testGrid() throws IOException {
+		pmr("testGrid()");
 	}
 
-	public void runTests() {
+	public static void runGridTest() throws IOException {
+		pmr("new GridTest()");
+		new GridTest().ReadGrid(TESTFILE);
+	}
+
+	public void runTests() throws IOException{
 		testGrid();
-
+		runGridTest();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		new ConwayGameOfLifeTest().runTests();
 	}
 }
